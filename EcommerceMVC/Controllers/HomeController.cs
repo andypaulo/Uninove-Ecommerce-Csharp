82559a5a062d1d6587c2033361d3597e21bee9da
Anderson Paulo <anderfilhopaulo@gmail.com>
2026-03-30 23:18:39 -0300

    Configuração banco de dados Banco de dados testado, funcionando Tela provisória para visualização de produtos

--- EcommerceMVC/Controllers/HomeController.cs
+++ EcommerceMVC/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using EcommerceMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.Data.Sqlite;
 
 namespace EcommerceMVC.Controllers
 {
@@ -8,8 +9,43 @@ namespace EcommerceMVC.Controllers
     {
         public IActionResult Index()
         {
+            var listaProdutos = new List<Produto>();
+            SQLitePCL.Batteries_V2.Init();
+            string connectionString = "Data Source=database/database.db;";
+            try
+            {
+                using (var connection = new SqliteConnection(connectionString))
+                {
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandText = "SELECT id, nome, descricao, preco, estoque, imagem FROM Produto";
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            listaProdutos.Add(new Produto
+                            {
+                                Id = reader.GetInt32(0),
+                                Nome = reader.GetString(1),
+                                Descricao = reader.IsDBNull(2) ? "" : reader.GetString(2),
+                                Preco = reader.GetDecimal(3),
+                                Estoque = reader.GetInt32(0),
+                                Imagem = reader.IsDBNull(4) ? "sem-foto.jpg" : reader.GetString(4)
+                            });
+                        }
+                    }
+                }
+
+                return View(listaProdutos);
+            }
+
+
+            catch (Exception ex)
+            {
+                ViewBag.DbState = "Erro: " + ex.Message;
+            }
             ViewData["Message"] = "Projeto de E-commerce iniciado!";
-            return View();
+            return View(new List<Produto>());
         }
 
         public IActionResult Privacy()
